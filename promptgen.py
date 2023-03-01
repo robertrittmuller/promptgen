0057a138baef5f2ad0f6b785672d632e62f150e3
Robert Rittmuller <robert@rittmuller.com>
2023-03-01 12:43:06 -0500

    Made random wildcard selection the default

--- promptgen.py
+++ promptgen.py
@@ -1,14 +1,57 @@
-pre_prompt = 'Photorealistic close-up portrait photo of a abstract sculpture with'
-post_prompt = 'surreal, fog, epic cybercity background, wet pavement, soft focus, moody lighting, neon lights, neon signs, cyberpunk, depth of field'
-base_negative_prompt = 'art, cartoon, painting, oil, trending on artstation, render, octane'
-base_resolution = ['-W1280 -H1024', '-W1024 -H1280']
-base_settings = '-n 5 -s 75 -G .75 -U 4 --hires_fix'
-adjectives = ['black shiny surface','red dimpled surface','multicolor surface, with potmarks','purple textured surface']
-samplers = ['k_lms','k_euler_a','k_heun']
-cfg = [7.5, 8.5, 9, 11]
+import re
+import csv
+import random
 
-for adj in adjectives:
-    for samp in samplers:
-        for cg in cfg:
-            for res in base_resolution:
-                print(f'{pre_prompt}, {adj}, {post_prompt}, [{base_negative_prompt}] -A{samp} -C{cg} {res} {base_settings}')
+# default filenames/paths
+template_path = "templates.csv"
+prompt_path = "prompts.txt"
+
+# default options
+number_of_sets = 5
+base_resolutions = ['-W1600 -H1024']
+base_settings = '-n 3 -s 150 -U 4 --hires_fix'
+samplers = ['k_heun']
+cfg = [7.5,8.5,9.5]
+
+# read in the prompt templates
+templates = []
+with open(template_path, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    for row in csvreader:
+        templates.append(row)
+
+# read in the prompts
+prompts = []
+with open(prompt_path, newline='') as promptfile:
+    for row in promptfile:
+        prompts.append(row.strip())
+
+# process each prompt for any templates
+for each_prompt in prompts:
+    for i in range(number_of_sets):
+        final_prompt = each_prompt
+        for each_template in templates:
+            search_text = '{' + each_template[0] + '}'
+            final_prompt = final_prompt.replace(search_text, each_template[1])
+        
+        # now we need to generate all of the varients if there are any wildcard blocks
+        wildcard_blocks = re.findall(r'\(.*?\)', final_prompt)
+        
+        for each_wildcard_block in wildcard_blocks:
+            # decompose each block down to individual elements based on the pipe | delimiter
+            this_wildcard_block = each_wildcard_block.replace('(', '')
+            this_wildcard_block = this_wildcard_block.replace(')', '')
+            block_elements = this_wildcard_block.split('|')
+            
+            # choose one random element to use for this prompt
+            this_element = random.choice(block_elements)
+
+            # drop in the selected element
+            final_prompt = final_prompt.replace(each_wildcard_block, this_element.strip())
+        
+        # now render the final prompt with options
+        for samp in samplers:
+            for cg in cfg:
+                for res in base_resolutions:
+                    print(f' {final_prompt} -A{samp} -C{cg} {res} {base_settings}')
+    
